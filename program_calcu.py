b22a8b4e3936d2765c183151d49951765f0d516e
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-06-03 19:57:22 +0800

    import calculator, then add a name feature

--- program_calcu.py
+++ program_calcu.py
@@ -19,7 +19,6 @@ print(Fore.LIGHTMAGENTA_EX+' THIS PROGRAM WILL HELP YOU ADD, SUBTRACT, MULTIPLY 
 print(border)
 time.sleep(2)
 
-
 try:
     print ( " You may choose your desired Operation!\n A. Addition\n S. Subtraction\n M. Multiplication\n D. Division ")
 
--- simple_calcu_inherit.py
+++ simple_calcu_inherit.py
@@ -1,3 +1,7 @@
-class SimpleCalcuInherit:
-    pass
+from oop_simplecalcu import Calculator
+
+class SimpleCalcuInherit(Calculator):
+    def user_name(self):
+        name_user = input(" What is your name? ")
+        print(" Hi,", name_user)
     
